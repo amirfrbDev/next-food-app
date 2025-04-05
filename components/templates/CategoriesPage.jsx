8577fd1c0b0d1038ea179a917c52576347f33240
Amir <amirmahdifarajbakhsh313@gmail.com>
2025-04-05 19:03:20 +0330

    filtering logic added

--- components/templates/CategoriesPage.jsx
+++ components/templates/CategoriesPage.jsx
@@ -1,18 +1,28 @@
 import { useState } from "react"
 import styles from "./CategoriesPage.module.css"
+import { useRouter } from "next/router"
 
-function CategoriesPage() {
+function CategoriesPage(props) {
+    // console.log(props)
 
     const [query, setQuery] = useState({
         difficulty: "",
         time: ""
     })
 
+    const router = useRouter()
+
     const changeHandler = (e) => {
         const value = e.target.value;
         const name = e.target.name
 
         setQuery(queries => ({ ...queries, [name]: value }))
+    }
+
+    const searchHandler = () => {
+        // if (!query.difficulty || !query.time) return
+
+        router.push({ pathname: "/categories", query })
     }
 
     return (
@@ -22,14 +32,14 @@ function CategoriesPage() {
                 <div className={styles.select}>
                     <select name="difficulty" value={query.difficulty} onChange={changeHandler}>
                         <option value="">Difficulty</option>
-                        <option value="">Easy</option>
-                        <option value="">Medium</option>
-                        <option value="">Hard</option>
+                        <option value="easy">Easy</option>
+                        <option value="medium">Medium</option>
+                        <option value="hard">Hard</option>
                     </select>
                     <select name="time" value={query.time} onChange={changeHandler}>
                         <option value="">Cooking Time</option>
-                        <option value="">More than 30 mins</option>
-                        <option value="">Less than 30 mins</option>
+                        <option value="less">Less than 30 mins</option>
+                        <option value="more">More than 30 mins</option>
                     </select>
                     <button onClick={searchHandler}>search</button>
                 </div>
--- pages/categories/index.jsx
+++ pages/categories/index.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
 import CategoriesPage from '../../components/templates/CategoriesPage'
 
-function Categories() {
-  return <CategoriesPage />
+function Categories({foods}) {
+  console.log(foods)
+  return <CategoriesPage {...foods} />
 }
 
 export default Categories
+
+
+
+export async function getServerSideProps({ query: { difficulty, time } }) {
+
+  const res = await fetch("http://localhost:4000/data");
+  const data = await res.json();
+
+  const filteredData = data.filter(food => {
+
+    const difficultyResult = food.details.filter(detail =>
+      detail.Difficulty && detail.Difficulty.toLowerCase() === difficulty.toLowerCase()
+    );
+
+    const timeResult = food.details.filter(detail => {
+      const cookingTime = detail["Cooking Time"] || "";
+      const detailTime = cookingTime.split(" ")[0];
+
+      if (detailTime && time === "less" && +detailTime <= 30) {
+        return true;
+      } else if (detailTime && time === "more" && +detailTime > 30) {
+        return true;
+      }
+      return false;
+    });
+
+    if (time && difficulty && timeResult.length && difficultyResult.length) {
+      return true;
+    } else if (time && !difficulty && timeResult.length) {
+      return true;
+    } else if (!time && difficulty && difficultyResult.length) {
+      return true;
+    }
+
+    return false;
+  });
+
+
+  return {
+    props: {
+      foods: filteredData
+    }
+  }
+
+}
